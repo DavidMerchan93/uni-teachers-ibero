65ad61abc010cb25752e8d43903df291b60f1d7c
David Merchan <davidmerchan10@gmail.com>
2026-04-16 23:13:45 -0500

    edited app.js to create docente form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,98 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [titulo, setTitulo] = useState('');
+  const [areaAcademica, setAreaAcademica] = useState('');
+  const [dedicacion, setDedicacion] = useState('');
+  const [aniosExperiencia, setAniosExperiencia] = useState(0);
+
+  const [registros, setRegistros] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+    cargarDocentes();
+  }, []);
+
+  const cargarDocentes = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/docentes');
+      const data = await response.json();
+      setRegistros(data);
+    } catch (error) {
+      alert('Error al cargar el listado de docentes :(');
+    }
+  };
+
+  const limpiarFormulario = () => {
+    setNombre('');
+    setCorreo('');
+    setTelefono('');
+    setTitulo('');
+    setAreaAcademica('');
+    setDedicacion('');
+    setAniosExperiencia(0);
+  };
+
+  const registrarDocente = async (e) => {
+    e.preventDefault();
+
+    const payload = {
+      nombre,
+      correo,
+      telefono,
+      titulo,
+      area_academica: areaAcademica,
+      dedicacion,
+      anios_experiencia: aniosExperiencia
+    };
+
+    if(editIndex !== null) {
+      try {
+        
+        const docente = registros[editIndex];
+        const response = await fetch(`http://localhost:3000/docentes/${docente.id}`,{
+          method: 'PUT',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(payload)
+        });
+
+        if(response.ok) {
+          const nuevoRegistro = [...registros];
+          nuevoRegistro[editIndex] = {
+            ...docente,
+            nombre,
+            correo,
+            telefono,
+            titulo,
+            area_academica: areaAcademica,
+            dedicacion,
+            anios_experiencia: aniosExperiencia
+          };
+
+          setRegistros(nuevoRegistro);
+          setEditIndex(null);
+          alert('Docuente actualizado con éxito');
+        }else{
+          const err = await response.json().catch(() => ({}));
+          alert(err.error || 'Error al actualizar el docente 1')
+        }
+
+      } catch (error) {
+        alert('Error al actualizar el docente 2')
+      }
+    } else {
+      // Ruta para crear el docente
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
